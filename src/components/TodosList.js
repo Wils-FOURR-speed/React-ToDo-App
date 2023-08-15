53628f358844ec78c4a81e2c2da81ed7335e4e28
Wils-FOURR-speed <124159862+Wils-FOURR-speed@users.noreply.github.com>
2023-08-15 12:43:55 +0100

    Building it up

---
+++ src/components/TodosList.js
@@ -0,0 +1,11 @@
+import TodoItem from './TodoItem';
+const TodosList = ({ todosProps }) => {
+  return (
+    <ul>
+      {todosProps.map((todo) => (
+        <TodoItem itemProp={todo} />
+      ))}
+    </ul>
+  );
+};
+export default TodosList;
